1e10fe0ef71d378a18fcd7cdff3ab1f10b5c8eff
Robert Sainton <robertsainton@outlook.com>
2023-02-06 22:45:52 -0500

    assignment completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(review_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.message}"}, status: :not_found
+  end
+
+  def review_params
+    params.permit(:name, :description, :price)
   end
 
 end
